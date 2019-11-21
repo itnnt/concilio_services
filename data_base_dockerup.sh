d5f334e8f56f01a724fccfaee262fb464aba6143
tungnguyen <tung.nn83@gmail.com>
2019-11-21 18:07:07 +0700

    add libs add connection to postgres docker

--- data_base_dockerup.sh
+++ data_base_dockerup.sh
@@ -1,3 +1,3 @@
-docker run --name postgres -e POSTGRES_USER=dbapplication_user -e POSTGRES_PASSWORD=sOKJuhssuyUY -d -p 5432:5432 postgres
+docker run --name postgres -e POSTGRES_USER=dbapplication_user -e POSTGRES_PASSWORD=sOKJuhssuyUY -d -p 5432:5432 postgres -c max_connections=200 -c shared_buffers=100MB
 docker run --detach --name mysql -e MYSQL_ROOT_PASSWORD=justapassword --publish 3306:3306 mysql
 docker run --detach --name=mysql --env="MYSQL_ROOT_PASSWORD=justapassword" -p 3306:3306 mysql
